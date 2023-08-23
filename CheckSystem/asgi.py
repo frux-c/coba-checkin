06749aaa8fe7c63371ff2ccb049c4e7452d3efcb
Chaka <agchaka@miners.utep.edu>
2023-08-23 16:37:27 -0600

    inital commit

---
+++ CheckSystem/asgi.py
@@ -0,0 +1,16 @@
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from . import routing
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CheckSystem.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
