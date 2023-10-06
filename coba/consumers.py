05eb1953c8365a337e297afe76fc37b66f309890
frux-c <frux.infoc@gmail.com>
2023-10-06 11:58:55 -0600

    consumer update to keep connection alive

--- coba/consumers.py
+++ coba/consumers.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from channels.consumer import AsyncConsumer
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
@@ -10,22 +11,40 @@ import json
 class CheckInConsumer(AsyncJsonWebsocketConsumer):
 	GROUP_NAME = 'events'
 	async def connect(self): # called when the websocket is handshaking as part of initial connection
+		# accept the connection
+		await self.accept()
+		# add user to group
 		await self.channel_layer.group_add(
 			self.GROUP_NAME,
 			self.channel_name
 		)
-		await self.accept()
+		# start keep alive task
+		self.keep_alive_task = asyncio.ensure_future(self.send_keep_alive())
 
 	async def disconnect(self,close_code): # called when the websocket closes
-		self.close(close_code)
+		# discard user from group
+		await self.channel_layer.group_discard(
+			self.GROUP_NAME,
+			self.channel_name
+		)
+		# cancel keep alive task
+		self.keep_alive_task.cancel()
 
 	async def receive(self,text_data): # called when the client sends message
-		print(f"channel name: {self.channel_name}")
-		print(f"incoming message: {text_data}")
+		print(
+			f"User: {self.scope['user']}",
+			f"CheckInConsumer:receive: {text_data}"
+		)
 
 	async def send_group_message(self,res):
 		del res['type']
 		await self.send(text_data=json.dumps(res)) # send message to websocket
+
+	async def send_keep_alive(self):
+		while True:
+			await asyncio.sleep(30)
+			await self.send(text_data=json.dumps({'message':'keep_alive', 'type':'keep_alive'}))
+		
 
 	def __str__(self):
 		return f"CheckInConsumer({self})"
