a105c730ab0c3ece15142eb27039828c993628f6
frux-c <frux.infoc@gmail.com>
2024-10-09 16:23:01 -0600

    fix utils 1

--- coba/utils.py
+++ coba/utils.py
@@ -3,7 +3,7 @@ import datetime
 from django.template import Template, Context
 
 def calculate_delta(row):
-    if pd.notna(row['checkout_time']):
+    if pd.notna(row['auto_time_in']) and pd.notna(row['auto_time_out']):
         checkin_dt = datetime.datetime.combine(datetime.datetime.today(), row['auto_time_in'])
         checkout_dt = datetime.datetime.combine(datetime.datetime.today(), row['auto_time_out'])
         return (checkout_dt - checkin_dt).seconds / 3600  # Convert to hours
